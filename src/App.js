bdb0c0911e2015698055b5cac8588318526cfea9
Yura <yury.sudak@gmail.com>
2022-01-22 22:28:05 +0300

    starting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Board from "./components/Board";
+import React from "react";
+
+const data = [
+    {title: 'Сделать',
+    items: [
+        {
+            id: 1,
+            description: 'Сходить в тренажёрку',
+        },
+        {
+            id: 2,
+            description: 'Сходить в магазин',
+        },
+        {
+            id: 3,
+            description: 'Забрать Артёма из садика',
+        }
+    ]},
+    {title: 'Сегодня',
+    items: [
+    {
+        id: 4,
+        description: 'Сделать задание по программированию',
+    },
+    {
+        id: 5,
+        description: 'Купить варочную панель',
+    },
+    ]}
+]
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Board data = {data} />
       </header>
     </div>
   );
